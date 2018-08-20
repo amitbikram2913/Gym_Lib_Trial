2d815381494c6f9b1b0efd7324f4fc8758c0e383
amitbikram2913 <amitb441@gmail.com>
2018-08-20 16:34:38 +0530

    Update __init__.py

--- Gym_Lib_Trial/__init__.py
+++ Gym_Lib_Trial/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
 
+register(
+    id='tri-v0',
+    entry_point='Gym_Lib_Trial.envs:TriEnv',
+)
+register(
+    id='tri-extrahard-v0',
+    entry_point='Gym_Lib_Trial.envs:TriExtraHardEnv',
+)
